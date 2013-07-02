84d265a000e1f6027b7a2d90f5a32b24441b7e71
Ah-Lun Tang <tang@ahlun.be>
2013-07-02 14:34:37 +0200

    functions for repos and issues

--- index.php
+++ index.php
@@ -3,10 +3,37 @@
 
 
 require_once 'vendor/autoload.php';
+require_once 'config.php';
 
 $client = new Github\Client();
-$repositories = $client->api('user')->repositories('oSoc13');
+$client->authenticate($token, $password, Github\Client::AUTH_URL_CLIENT_ID);
 
-foreach ($repositories as $repo) {
-	echo $repo['name'];
+
+get_repos($client,'oSoc13');
+
+
+function get_repos($client, $username){
+    try {  
+        $repositories = $client->api('user')->repositories($username);
+        foreach ($repositories as $repo) {
+            $reponame = $repo['name'];
+            echo $reponame."<br />";
+            get_issues($client,$username, $reponame);
+        }
+    } catch (Exception $e) {  
+        # do something with e
+    } 
+}
+
+
+function get_issues($client, $username, $reponame){
+
+    try { 
+
+        $issues = $client->api('issue')->all('oSoc13', $reponame, array('state' => 'open'));
+
+    } catch (Exception $e) {  
+        # do something with e
+    } 
+ 
 }
